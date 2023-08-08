d1fb65630ed065717a5c6c650a6b9786c35839b7
sidhartha2002 <mohantysidhartha2002@gmail.com>
2023-08-08 16:36:06 +0530

    Add via Uploads 🚀

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-blackish text-gray-500 text-center py-4 pb-16 md:pb-4">
+      Copyright © InstaMart <br /> All Rights Reserved 2023.
+    </div>
+  );
+};
+
+export default Footer;
